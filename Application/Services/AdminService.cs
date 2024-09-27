b305ffb72ede028862abc9a01a773e11126a69a3
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-09-27 11:27:49 -0300

    Creada rama Admin - Creada entidad Admin

---
+++ Application/Services/AdminService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminService(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+        public List<Admin> GetAll()
+        {
+            return _adminRepository.Get();
+        }
+
+        public Admin AddAdmin(Admin admin)
+        {
+            return _adminRepository.Add(admin);
+        }
+    }
+}
