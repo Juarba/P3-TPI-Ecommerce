ebc7bfab530f7e296d54a100a4a9f409164bd099
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-10-04 17:48:06 -0300

    Product

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public ProductRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Product? Get(string name)
+        {
+            return _context.Products.FirstOrDefault(p => p.Name == name);
+        }
+
+        public List<Product> GetProductByName(string name)
+        {
+            return _context.Products.Where(x => x.Name == name).ToList();
+        }
+    }
+}
---
+++ TPI-Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace TPI_Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.GetAll());
+        }
+    }
+}
