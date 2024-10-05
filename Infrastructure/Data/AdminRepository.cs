aec81958e5832e262adad5948148ee0520caf6c4
lucademinas <130857037+lucademinas@users.noreply.github.com>
2024-10-05 11:03:07 -0300

    Revert "cambios en admin repository"

--- Infrastructure/Data/AdminRepository.cs
+++ Infrastructure/Data/AdminRepository.cs
@@ -16,36 +16,9 @@ namespace Infrastructure.Data
             _context = context;
         }
 
-        public Admin? GetAdminByName(string name)
+        public List<Admin> GetAdminByName(string name)
         {
-            return _context.Admins.FirstOrDefault(x => x.Name==name);
+            return _context.Admins.Where(x => x.Name == name).ToList();
         }
-
-        public Admin Add(Admin admin)
-        {
-            _context.Admins.Add(admin);
-            _context.SaveChanges();
-            return admin;
-        }
-
-        public void Delete(Admin admin) 
-        {
-        _context.Remove(admin);
-        _context.SaveChanges();
-        }
-
-        public List<Admin> GetAll() 
-        { 
-            return _context.Admins.ToList();
-        }
-        public void Update(Admin admin)
-        {
-            _context.Update(admin);
-            _context.SaveChanges();
-        }
-        public void SaveChanges()
-        {
-            _context.SaveChanges();
-        }
-    }   
+    }
 }
